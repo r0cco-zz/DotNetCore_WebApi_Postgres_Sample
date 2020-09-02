67d9087a660035d92af4e97b9e82f32e62d19308
Rocco Riccardi <rriccardi@portfoliopathway.com>
2020-09-02 16:24:50 -0500

    uncommented out using statement

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npgsql;
-//using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyWebApi
 {
